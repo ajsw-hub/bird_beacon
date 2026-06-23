c026d6ad5f0cd05b87db4d2301f1e7d72fb69a3c
Adrian <adrianjswaine@gmail.com>
2026-06-23 14:34:05 +0100

    adding V1 migration creating tables for users and posts@

---
+++ src/main/resources/db/migration/V1__creating_tables_for_database.sql
@@ -0,0 +1,22 @@
+CREATE TABLE users (
+    id BIGSERIAL PRIMARY KEY,
+    username VARCHAR(100),
+    email VARCHAR(100),
+    enabled BOOLEAN
+);
+
+INSERT INTO users (username, email, enabled) VALUES
+('super_cooper','super@cooper.com', TRUE),
+('test','test@test.com', TRUE);
+
+CREATE TABLE posts(
+    id BIGSERIAL PRIMARY KEY,
+    content VARCHAR(300),
+    posterId BIGINT NOT NUll REFERENCES users(id) ON UPDATE CASCADE
+);
+
+INSERT INTO posts (content, posterId) VALUES
+('Wow, I saw a bird', 1),
+('That bird be large',2),
+('I saw another bird!', 1),
+('That bird is even larger!',2);
