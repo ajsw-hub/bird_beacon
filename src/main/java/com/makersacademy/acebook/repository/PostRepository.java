9a3008d9d8963b185aca415b66609efbb1755561
Adrian <adrianjswaine@gmail.com>
2026-06-24 16:43:25 +0100

    added profile page where the user can see a list of only their posts. Added logout buttons.

--- src/main/java/com/makersacademy/acebook/repository/PostRepository.java
+++ src/main/java/com/makersacademy/acebook/repository/PostRepository.java
@@ -3,6 +3,10 @@ package com.makersacademy.acebook.repository;
 import com.makersacademy.acebook.model.Post;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface PostRepository extends CrudRepository<Post, Long> {
+
+    public List<Post> findByPosterid(Long userid);
 
 }
