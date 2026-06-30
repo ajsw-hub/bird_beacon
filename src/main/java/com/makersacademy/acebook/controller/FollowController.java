c5284eb84a15a1b3cfa4ff35582030c3eb0e5647
mpmua <mgr_55@hotmail.com>
2026-06-30 16:02:14 +0100

    add logic to follow controller which handles follow requests from users

--- src/main/java/com/makersacademy/acebook/controller/FollowController.java
+++ src/main/java/com/makersacademy/acebook/controller/FollowController.java
@@ -1,37 +1,42 @@
 package com.makersacademy.acebook.controller;
 
+import com.makersacademy.acebook.model.Follow;
 import com.makersacademy.acebook.model.Post;
+import com.makersacademy.acebook.model.User;
+import com.makersacademy.acebook.repository.FollowRepository;
 import com.makersacademy.acebook.repository.PostRepository;
 import com.makersacademy.acebook.repository.UserRepository;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.Optional;
 
 @RestController
 public class FollowController {
     @Autowired
-    PostRepository postRepository;
-
-    @Autowired
     UserRepository userRepository;
 
-    @PostMapping("/follow")
-    public RedirectView create (@ModelAttribute Post post, HttpSession session, @RequestParam String redirect){
+    @Autowired
+    FollowRepository followRepository;
 
-//        String username = (String) session.getAttribute("username");
-//
-//        Optional<User> findingUser = userRepository.findUserByUsername(username);
-//        User currentUser = findingUser.get();
-//
-//        post.setPosterid(currentUser.getId());
+    @PostMapping("/follow/{followedUserId}")
+    public RedirectView create (HttpSession session, @PathVariable Long followedUserId){
 
-//        postRepository.save(post);
+          String username = (String) session.getAttribute("username");
 
-        return new RedirectView(redirect);
+          Optional<User> findingUser = userRepository.findUserByUsername(username);
+          User loggedInUser = findingUser.get();
+          Long loggedInUserId = loggedInUser.getId();
+
+          Follow follow = new Follow();
+          follow.setFollowerid(loggedInUserId);
+          follow.setFollowingid(followedUserId);
+
+          followRepository.save(follow);
+
+          return new RedirectView("/birdfeed");
     }
 
 //    @PostMapping("/unfollow")
