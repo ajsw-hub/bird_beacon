62a400bc05da331ca7d1cd5864b20b2c07f64296
mpmua <mgr_55@hotmail.com>
2026-07-01 16:05:17 +0100

    added followers to my flock page with links to each followers profile

--- src/main/java/com/makersacademy/acebook/controller/FlockController.java
+++ src/main/java/com/makersacademy/acebook/controller/FlockController.java
@@ -7,10 +7,13 @@ import com.makersacademy.acebook.repository.UserRepository;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@RestController
 public class FlockController {
 
     @Autowired
@@ -22,11 +25,21 @@ public class FlockController {
     @GetMapping("/my-flock")
     public ModelAndView myFlock(HttpSession session) {
 
+        ModelAndView myFlockView = new ModelAndView("/my-flock");
         String username = session.getAttribute("username").toString();
         User currentUser = userRepository.findUserByUsername(username).get();
 
         System.out.println("My flock: " + followRepository.findFollowingIdsByFollowerId(currentUser.getId()));
         List<Long> myFlock = followRepository.findFollowingIdsByFollowerId(currentUser.getId());
-        return new ModelAndView("/myflock");
+
+        ArrayList<User> myFlockUsers = new ArrayList<User>();
+        for (Long user: myFlock) {
+            myFlockUsers.add(userRepository.findById(user).get());
+        }
+
+        myFlockView.addObject("myFlockUsers", myFlockUsers);
+        System.out.println("My flock: " + myFlockUsers);
+
+        return myFlockView;
     }
 }
